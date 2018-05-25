4dc429110ab2d0f29e804ddb21b65c08efd8da3f
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-25 20:50:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,6 +30,17 @@ class Scraper
       end
     end
 
+    def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
 
 # Scraper.new.get_page
@@ -37,15 +48,6 @@ end
 
 
 
-#     def print_courses
-#     self.make_courses
-#     Course.all.each do |course|
-#       if course.title
-#         puts "Title: #{course.title}"
-#         puts "  Schedule: #{course.schedule}"
-#         puts "  Description: #{course.description}"
-#       end
-#     end
-#   end
+
 #
 # end
