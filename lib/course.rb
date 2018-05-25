3ce494ba935846be6885f22c47abb41e8e937ce1
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-25 20:16:34 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,5 +2,18 @@ class Course
 
   attr_accessor :title, :schedule, :description
 
-  
+  @@all = []
+
+  def initialize
+    @@all  << self
+  end
+
+  def self
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
 end
